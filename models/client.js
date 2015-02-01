c4e200d10671b39e924a3b4574d277f9de4243a5
Stephen Doherty <stemado@users.noreply.github.com>
2015-02-01 17:29:45 -0600

    Created client.js
    First attempt at schema listed by djohnson that would be needed.

---
+++ models/client.js
@@ -0,0 +1,24 @@
+//NOTE: Always place children schemas above the parent
+//this seems to be necessary if you want embedded children to contain their own _id property
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var organizationSchema = Schema({
+    org_name: { type: String, required: true, trim: true, unique: true }, //UNIQUE
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+}, {
+    _id: true
+});
+
+var clientSchema = Schema({
+    ee_dob: { type: String, required: true, trim: true },
+    ee_zip: { type: String, required: true, trim: true },
+    organization: [organizationSchema],
+    created_at: { type: Date, default: Date.now },
+    updated_at: { type: Date, default: Date.now }
+});
+
+
+module.exports = mongoose.model('Account', orgSchema);
